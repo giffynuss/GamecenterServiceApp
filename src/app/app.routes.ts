8a1bc32f74a8ac9de4eeb6484990c867e1eab943
unknown <EDU22_008@EDU4PDB.DE>
2025-06-11 16:01:26 +0200

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        redirectTo: 'app',
+        pathMatch: 'full'
+    },
+    {
+        path : 'app',
+        component: App
+    },
+    {
+        path : '**',
+        redirectTo: 'app'
+    }
+];
