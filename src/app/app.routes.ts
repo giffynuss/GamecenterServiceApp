7184429503bd4ff99b6a1283ba136359a4de98e0
Patryk <giffygang.business@gmail.com>
2025-06-12 22:39:02 +0200

    DonnerstagLastPush

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Home } from './home/home';
 import { Places } from './places/places';
 import { PlaceDetails } from './place-details/place-details';
 import { Login } from './login/login';
+import { Service } from './service/service';
+import { Stages } from './stages/stages';
 
 export const routes: Routes = [
     {
@@ -25,5 +27,13 @@ export const routes: Routes = [
     {
         path : 'place-details/:id',
         component: PlaceDetails
+    },
+    {
+        path : 'service',
+        component: Service
+    },
+    {
+        path : 'stages',
+        component: Stages
     }
 ];
