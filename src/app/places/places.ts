08ace7a0c9f9815749e9fbbe5b96301ab2e1ea6b
unknown <EDU22_007@EDU4PDB.DE>
2025-06-12 12:31:09 +0200

    sheeesh

--- src/app/places/places.ts
+++ src/app/places/places.ts
@@ -1,20 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { Place } from '../place';
 import { Placeservice } from '../placeservice';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-places',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './places.html',
   styleUrl: './places.css'
 })
 export class Places {
 
+  place: Place | null = null;
   places: Place[] = [];
   personService: Placeservice = inject(Placeservice);
 
   constructor() {
     this.places = this.personService.getPlaces();
+  }
+
+  toggleOccupation(place: Place) {
+   place.occupation = !place.occupation;
   }
 
 }
--- src/app/placeservice.ts
+++ src/app/placeservice.ts
@@ -10,11 +10,26 @@ export class Placeservice {
   
     constructor() {
       this.places = [
-        { id: 1, price: 0, occupation: false,},
+        { id: 1, price: 0, occupation: false },
         { id: 2, price: 0, occupation: false },
         { id: 3, price: 0, occupation: false },
         { id: 4, price: 0, occupation: false },
-        { id: 5, price: 0, occupation: false }
+        { id: 5, price: 0, occupation: false },
+        { id: 6, price: 0, occupation: false },
+        { id: 7, price: 0, occupation: false },
+        { id: 8, price: 0, occupation: false },
+        { id: 9, price: 0, occupation: false },
+        { id: 10, price: 0, occupation: false },
+        { id: 11, price: 0, occupation: false },
+        { id: 12, price: 0, occupation: false },
+        { id: 13, price: 0, occupation: false },
+        { id: 14, price: 0, occupation: false },
+        { id: 15, price: 0, occupation: false },
+        { id: 16, price: 0, occupation: false },
+        { id: 17, price: 0, occupation: false },
+        { id: 18, price: 0, occupation: false },
+        { id: 19, price: 0, occupation: false },
+        { id: 20, price: 0, occupation: false },
       ];
     }
 
